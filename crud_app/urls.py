1ff6eadeacfda9465431eea31425a08a8b3f5817
naf <naf@auri.ga>
2021-01-15 14:16:48 +0900

    url変更

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.all_bmi, name='index'),
-    path('form',views.form, name='form')
+    path('',views.BmiView.as_view(), name='index'),
+    #path('form',views.form, name='form')
 ]
--- crudproject/urls.py
+++ crudproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('crud_app.urls'))
+    path('bmi/', include('crud_app.urls'))
 
 ]
