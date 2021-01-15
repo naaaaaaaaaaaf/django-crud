b2a0013e503107251606d24b0543e416a646dda0
naf <naf@auri.ga>
2021-01-15 14:17:15 +0900

    view変更

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .forms import BmiForm
+from .models import Bmi
 # Create your views here.
+class BmiView(ListView):
+    template_name = 'bmi/all.html'
+    model = Bmi
+
+# 関数ベースだとこんな感じになる
+def all_bmi(request):
+    params = {
+        'data': Bmi.objects.all()
+    }
+    return render(request,'bmi/all.html',params)
