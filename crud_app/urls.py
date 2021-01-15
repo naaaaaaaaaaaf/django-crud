2333e6d7bd1be30da6e761133ac174021a426afd
naf <naf@auri.ga>
2021-01-15 15:41:57 +0900

    CreateVIew

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.BmiView.as_view(), name='index'),
+        
+    path('create',views.CreateBmi.as_view(), name='create'),
+
     #path('form',views.form, name='form')
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .forms import BmiForm
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from .models import Bmi
+from django.urls import reverse_lazy
 # Create your views here.
 class BmiView(ListView):
     template_name = 'bmi/all.html'
     model = Bmi
+class CreateBmi(CreateView):
+    template_name = 'bmi/create.html'
+    model = Bmi
+    fields = ('name', 'bmi')
+    success_url = reverse_lazy('index')
 
 # 関数ベースだとこんな感じになる
 def all_bmi(request):
